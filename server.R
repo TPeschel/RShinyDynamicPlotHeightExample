662da2f9671aaf5be5c4c683040aba5297e6ef79
Unknown <tpeschel@life.uni-leipzig.de>
2019-07-10 15:23:03 +0200

    improve all

--- server.R
+++ server.R
@@ -1,21 +1,35 @@
-###
-# first: find by binary partition the right values for the height of the slider and text and min plot.height
-###
-# in my browser, maybe in all ;o)
-slider.height   <- 64 + 32 + 4                    # 100
-text.height     <- 2 * ( 16 + 4 )                 # 20
-min.plot.height <- 128 + 64 + 32 + 16 + 8 + 4 + 1 # 253
+analyse <- function( x ,level = .975 ) {
+	n <- length( x )
+	m <- mean( x )
+	v <- var( x )
+	q <- qt( level, n - 1 ) * sqrt( v / n )
+	list(
+		N    = n,
+		MEAN = m,
+		SD   = sqrt( v ),
+		LOW  = m - q, 
+		HIGH = m + q 
+	)
+}
+
+min.plot.height <- 253
+y.offset <- 160
 
 server <- function( input, output, session ) {
 	
-	# min.plot.height <- 253 was found by observing height when the plot crashes
+	rv <- reactiveValues( )
+	
+	rv$plot.time <- Sys.time( )
+	
 	screen.height <- function( ) {
 		
+		shinyjs::runjs( "$('*').each(function(i,e){console.log(i+' '+e)});" )
+				
 		print( input$resolution )
 		
 		i <-
 			ifelse(
-				is.null( input$resolution ) || input$resolution$height < min.plot.height + slider.height + text.height,
+				is.null( input$resolution ) || input$resolution$height < y.offset + min.plot.height,
 				min.plot.height,
 				as.integer( input$resolution$height )
 			)
@@ -25,25 +39,59 @@ server <- function( input, output, session ) {
 		i
 	}
 	
-	# show a scatter plot some of gauss distribution
-	output$myPlot <- shiny::renderPlot( {
+	observe( {
 		
-		plot( 1 : input$SLIDER_N, rnorm( input$SLIDER_N ) )	
+		#print( paste0( "Check Plotauftrag: ", Sys.time( ) - rv$plot.time ) )
+		dt <- Sys.time( ) - rv$plot.time
+		if( 5 < dt ) {
+			
+			#print( "plotte" )
+			x <- isolate( qnorm( 1 : input$n / ( input$n + 1 ), input$mu, input$sigma ) )
+			y <- isolate( rnorm( input$n, input$mu, input$sigma ) )
+			s <- sort( y )
+			z <- s - x
+			a <- analyse( z )
+			print( a )
+			
+			output$plot <- renderPlot( 
+				{
+					par( mfrow = c( 2, 2 ) )
+					plot( y )
+					hist( y )
+					plot( x, s )
+					abline( 0, 1 )
+					plot( x, z ) 
+					abline( 0, 0 )
+					par( mfrow = c( 1, 1 ) )
+				}
+			)
+			
+			output$myPlotUI <- renderUI( {
+				
+				plotOutput( "plot", height = screen.height( ) - y.offset )	
+			} )
+			
+			updateProgressBar( session, "progress", 5., 5., "plot finished", "info" )
+		}
+		else {
+			
+			invalidateLater( 100 )
+			
+			updateProgressBar( session, "progress", dt, 5., "... left to plot", "success" )
+			
+			return( )
+		}
 	} )
 	
-	# render the plot with dynamic height
-	output$myPlotUI <- shiny::renderUI( {
-		
-		shiny::plotOutput(
-			"myPlot",
-			height = screen.height( ) - slider.height - text.height
-		)	
-	} )
-	
-	# just some text
-	# prints the resolution
-	output$resolution_text <- renderText( {
-		
-		paste0( "width: ", input$resolution$width, "   height: ", input$resolution$height )
-	} )
+	observeEvent(
+		eventExpr = {
+			c( input$n, input$mu, input$sigma )# input$resolution$height )
+		},
+		handlerExpr = {
+			
+			rv$plot.time <- Sys.time( )
+			
+			updateProgressBar( session, "progress", 0., 5., "start to plot", "primary" )
+		}
+	)
 }
--- ui.R
+++ ui.R
@@ -1,62 +1,54 @@
-###
-# build an app with a slider on top, a plot in the middle and a text output at the window's bottom
-# the plot's height should react on the browser window's height
-###
+library( shiny )
+library( shinyjs )
+library( shinyWidgets )
 
-ui <- shiny::fluidPage(
+ui <- fluidPage(
 	
-	title = "RShinyDynamicPlotHeightExample",
+	shinyjs::useShinyjs( ),
 	
-	###
-	# combining
-	# https://ryanve.com/lab/dimensions/
-	# and
-	# https://stackoverflow.com/questions/36995142/get-the-size-of-the-window-in-shiny
-	# gives something like that
-	###
 	shiny::tags$head(
 		shiny::tags$script(
-"
-// define a function that returns a struct named resolution
-// containing width and height of the inner window
-// accessible in shiny as a list named input$resolution
-// which elements are
-// width:  input$resolution$width
-// height: input$resolution$height
-function
-resized( e ) {
-	Shiny.onInputChange( 
-		'resolution', 
-		{ 
-			width  : window.innerWidth,
-			height : window.innerHeight
-		}
-	);
-}
-
-// call the function resized when shiny connects
-$(document).on(
-	'shiny:connected',
-	resized
-);
-
-// call the function resized when the window resizes
-$( window ).resize(
-	resized
-);
-"
+			"
+			// define a function that returns a struct named resolution
+			// containing width and height of the inner window
+			// accessible in shiny as a list named input$resolution
+			// which elements are
+			// width:  input$resolution$width
+			// height: input$resolution$height
+			function resized( e ) {
+				Shiny.onInputChange( 
+					'resolution', 
+					{ 
+						width  : window.innerWidth,
+						height : window.innerHeight
+					}
+				);
+			}
+			
+			function printDom( ) {
+				$('*').each(function(i,e){console.log(i+' '+e)});
+			}
+			
+			// call the function resized when shiny connects
+			$( document ).on(
+				'shiny:connected',
+				resized
+			);
+			
+			// call the function resized when the window resizes
+			$( window ).resize(
+				resized
+			);
+			"
 		)
 	),
-	
 	fluidRow(
-		# add a sliderInput to have something above the plot
-		column( 6, shiny::sliderInput( "SLIDER_N", "count of random numbers", 100, 1000, 10 ) ),
-		column( 6, shiny::tags$a( "Code on GitHub", href = "https://github.com/TPeschel/RShinyDynamicPlotHeightExample" ) )
+		column( width = 4, offset = 0, sliderInput( "n",     "#obs",  1000, 100000, 1000, 1000 ) ),
+		column( width = 4, offset = 0, sliderInput( "mu",    "mu",    1, 10, 1 ) ),
+		column( width = 4, offset = 0, sliderInput( "sigma", "sigma", 1, 10, 1 ) )
 	),
 	
-	# add a plot with dynamic height
 	shiny::uiOutput( "myPlotUI" ),
-		
-	# add a textOutput to have something below the plot
-	shiny::textOutput( "resolution_text" )
+	
+	progressBar( "progress", 0, 5, T, size = 'sm', "primary", T, unit_mark = "s" )
 )
